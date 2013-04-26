b9278a4b6197ac907f30465a320c2cd076444e6c
kentsay <naturen@gmail.com>
2013-04-26 15:46:04 +0800

    init Lawnmower

---
+++ src/com/yahoo/twshopping/codejam/Lawnmower.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.yahoo.twshopping.codejam;
+
+/**
+ * Description of the class.
+ *
+ * @author $Id: kentsay$
+ * @version $Revision: 1$
+ *
+ */
+public class Lawnmower {
+
+}
